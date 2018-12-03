d365309cfb612b42f9d81b4d899ea99c97c9ba7d
SugarShootingStar <ryu.8224@gmail.com>
2018-12-03 19:03:08 +0900

    fic

---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="container">
+    <!-- 雪を降らせる -->
+    <SnowFall/>
+    <div>
+      <logo/>
+      <h1 class="title">
+        particles-test
+      </h1>
+      <h2 class="subtitle">
+        My dandy Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+// 雪を降らせるコンポーネントの読み込み
+import SnowFall from '~/components/SnowFall'
+
+export default {
+  components: {
+    Logo,
+    SnowFall
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  /* 雪がよく見えるようにしたいので背景色黒 */
+  background: black;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
